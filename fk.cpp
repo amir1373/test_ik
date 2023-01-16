4b30697903a39cccd345ffca3fe6c588d7ffd1e4
SeyedAmirHossein Moshatghioun <moshtaghioun@gmail.com>
2023-01-16 10:42:03 +0330

    Update fk.cpp

--- fk.cpp
+++ fk.cpp
@@ -1,4 +1,28 @@
 #include "iostream"
+using namespace std;
+
+#define d1 0
+#define d2 0
+#define d3 0
+#define d4 0
+#define d5 0
+#define d6 0
+
+#define alpha1 0
+#define alpha2 0
+#define alpha3 0
+#define alpha4 0
+#define alpha5 0
+#define alpha6 0
+
+#define a1 0
+#define a2 0
+#define a3 0
+#define a4 0
+#define a5 0
+#define a6 0
+
+
 
 
 int main()
