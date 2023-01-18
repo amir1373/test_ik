388f9444b8e53e95637e334f1a97b06fcc4b1dce
SeyedAmirHossein Moshatghioun <moshtaghioun@gmail.com>
2023-01-18 12:28:13 +0330

    added matrix out put for each matrix

--- fk.cpp
+++ fk.cpp
@@ -13,12 +13,12 @@ using namespace Eigen;
 
 
 
-#define d1 450
+#define d1 .0450
 #define d2 0
 #define d3 0
-#define d4 640
+#define d4 0.640
 #define d5 0
-#define d6 95
+#define d6 0.095
 
 #define alpha1 (-90 * (PI/180))
 #define alpha2 (0   * (PI/180))
@@ -27,9 +27,9 @@ using namespace Eigen;
 #define alpha5 (-90 * (PI/180))
 #define alpha6 (0   * (PI/180))
 
-#define a1 150
-#define a2 570
-#define a3 130
+#define a1 0.150
+#define a2 0.570
+#define a3 0.130
 #define a4 0
 #define a5 0
 #define a6 0
@@ -88,6 +88,8 @@ T0_1 << cos(tetha1), (-1) * sin(tetha1)* cos(alpha1), sin(tetha1)* sin(alpha1)  
         sin(tetha1), cos(tetha1) * cos(alpha1)      , (-1) * cos(tetha1) * sin(alpha1) , a1 * sin(tetha1),
         0          , sin(alpha1)                    , cos(alpha1)                      , d1                  ,
         0          , 0                              , 0                                , 1                  ;
+        cout << "matrix output T0_1"; // Type a number and press enter
+cout << T0_1; // Type a number and press enter
 
 
 T1_2 << cos(tetha2), (-1) * sin(tetha2)* cos(alpha2), sin(tetha2)* sin(alpha2)         , a2 * cos(tetha2),
@@ -95,31 +97,38 @@ T1_2 << cos(tetha2), (-1) * sin(tetha2)* cos(alpha2), sin(tetha2)* sin(alpha2)  
         0          , sin(alpha2)                    , cos(alpha2)                      , d2                  ,
         0          , 0                              , 0                                , 1                   ;
 
+          cout << "matrix output T1_2"; // Type a number and press enter
+cout << T1_2; // Type a number and press enter      
+
 
 T2_3 << cos(tetha3), (-1) * sin(tetha3)* cos(alpha3), sin(tetha3)* sin(alpha3)         , a3 * cos(tetha3),
         sin(tetha3), cos(tetha3) * cos(alpha3)      , (-1) * cos(tetha3) * sin(alpha3) , a3 * sin(tetha3),
         0          , sin(alpha3)                    , cos(alpha3)                      , d3                  ,
         0          , 0                              , 0                                , 1                  ;
 
-
+          cout << "matrix output T2_3"; // Type a number and press enter
+cout << T2_3; // Type a number and press enter     
 
 T3_4 << cos(tetha4), (-1) * sin(tetha4)* cos(alpha4), sin(tetha4)* sin(alpha4)         , a4 * cos(tetha4),
         sin(tetha4), cos(tetha4) * cos(alpha4)      , (-1) * cos(tetha4) * sin(alpha4) , a4 * sin(tetha4),
         0          , sin(alpha4)                    , cos(alpha4)                      , d4                  ,
         0          , 0                              , 0                                , 1                   ;
 
-
+          cout << "matrix output T3_4"; // Type a number and press enter
+cout << T3_4; // Type a number and press enter     
 T4_5 << cos(tetha5), (-1) * sin(tetha5)* cos(alpha5), sin(tetha5)* sin(alpha5)         , a5 * cos(tetha5),
         sin(tetha5), cos(tetha5) * cos(alpha5)      , (-1) * cos(tetha5) * sin(alpha5) , a5 * sin(tetha5),
         0          , sin(alpha5)                    , cos(alpha5)                      , d5                  ,
         0          , 0                              , 0                                , 1                  ;
-
+          cout << "matrix output T4_5"; // Type a number and press enter
+cout << T4_5; // Type a number and press enter     
 
 T5_6 << cos(tetha6), (-1) * sin(tetha6)* cos(alpha6), sin(tetha6)* sin(alpha6)         , a6 * cos(tetha6),
         sin(tetha6), cos(tetha6) * cos(alpha6)      , (-1) * cos(tetha6) * sin(alpha6) , a6 * sin(tetha6),
         0          , sin(alpha6)                    , cos(alpha6)                      , d6                  ,
         0          , 0                              , 0                                , 1                   ;
-
+          cout << "matrix output T5_6"; // Type a number and press enter
+cout << T5_6; // Type a number and press enter     
 
 
 // T0_6 << cos(tetha6), (-1) * sin(tetha6)* cos(alpha6), sin(tetha6)* sin(alpha6)         , alpha6 * cos(tetha6),
